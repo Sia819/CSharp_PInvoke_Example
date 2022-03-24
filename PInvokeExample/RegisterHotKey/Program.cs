060ee7d7bc368dac5af259ff618e7bb2f5ad1346
Sia819 <lunasia819@gmail.com>
2022-03-25 03:27:18 +0900

    Add RegisterHotKey example

---
+++ PInvokeExample/RegisterHotKey/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using PInvokeExtension;
+using static PInvokeExtension.User32;
+using static PInvoke.Gdi32;
+using static PInvoke.User32;
+using static PInvoke.User32.ClassStyles;
+using static PInvoke.User32.Cursors;
+using static PInvoke.User32.WindowStyles;
+using static PInvoke.User32.WindowMessage;
+
+namespace RegisterHotKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegisterHotKey(0, 1, 0, 0x41); //Register A
+            MSG msg = { 0 };
+
+            while (GetMessageA(&msg, NULL, 0, 0) != 0)
+            {
+                if (msg.message == WM_HOTKEY)
+                {
+                    cout << "A"; //Print A if I pressed it
+                }
+            }
+
+            UnregisterHotKey(NULL, 1);
+            return 0;
+        }
+    }
+}
---
+++ PInvokeExample/RegisterHotKeyExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static Windows.define;
+using static PInvokeExtension.User32;
+using static PInvoke.User32;
+using static PInvoke.User32.WindowMessage;
+
+namespace RegisterHotKeyExample
+{
+    public unsafe class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            IntPtr hInstance = Marshal.GetHINSTANCE(typeof(Program).Module);
+
+            RegisterHotKey(NULL, 1, NULL, Keys.A);   // Register HotKey A
+            MSG _message = new MSG();                // point referenced of MSG structor
+            IntPtr Message = (IntPtr)(&_message);
+
+            while (GetMessage(Message, IntPtr.Zero, WM_NULL, WM_NULL) > 0)
+            {
+                TranslateMessage(Message);
+                DispatchMessage(Message);
+                if (_message.message == WM_HOTKEY)
+                {
+                    Console.WriteLine("A");
+                }
+            }
+            UnregisterHotKey(NULL, 1);
+        }
+    }
+}
